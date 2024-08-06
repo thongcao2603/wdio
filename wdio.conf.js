8e8ba95503a3069653bb91c5a5f1f1610983a3a1
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-08-07 04:03:14 +0700

    fix headless

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,7 +55,10 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu']
+        }
     }],
 
     //
