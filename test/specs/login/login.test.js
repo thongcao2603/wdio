a1d04c2ead3aa4e2e5ec4b2d5a5d466fc13abd73
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-08-07 04:06:42 +0700

    fix

--- test/specs/login/login.test.js
+++ test/specs/login/login.test.js
@@ -3,12 +3,12 @@ const loginPage = require('../../pages/login.page')
 
 
 describe('Test1', () => {
-    it('move mouse', async () => {
-        browser.url('/')
-        const ele = await $('div=SpiceClub')
-        await ele.moveTo()
-        await browser.pause(5000)
-    })
+    // it('move mouse', async () => {
+    //     browser.url('/')
+    //     const ele = await $('div=SpiceClub')
+    //     await ele.moveTo()
+    //     await browser.pause(5000)
+    // })
 
     it('move mouse2', async () => {
         browser.url('/')
