cf7464274e88831046ccd1420a20c61902d4ca2f
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-08-07 04:18:54 +0700

    test ci

--- test/specs/login/login.test.js
+++ test/specs/login/login.test.js
@@ -10,11 +10,16 @@ describe('Test1', () => {
     //     await browser.pause(5000)
     // })
 
+    // it('move mouse2', async () => {
+    //     browser.url('/')
+    //     const ele = await $('div=SpiceClub')
+    //     await ele.moveTo()
+    //     await browser.pause(5000)
+    // })
     it('move mouse2', async () => {
         browser.url('/')
-        const ele = await $('div=SpiceClub')
-        await ele.moveTo()
-        await browser.pause(5000)
+
+        expect(true).toBe(true)
     })
 })
 
--- wdio.conf.js
+++ wdio.conf.js
@@ -92,7 +92,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://www.spicejet.com/',
+    baseUrl: 'https://the-internet.herokuapp.com/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
